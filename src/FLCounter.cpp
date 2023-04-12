522e5c79f09a8241376e37f301a0763ef74975b2
MKLeo1 <94550363+MKLeo1@users.noreply.github.com>
2023-04-12 19:19:57 +0200

    Update FLCounter.cpp
    Corrected code control flow

--- src/FLCounter.cpp
+++ src/FLCounter.cpp
@@ -14,8 +14,8 @@
 
 namespace fs = boost::filesystem;
 
-struct DataInput;  // Holds the entered paths                                   
-struct DataOutput; // Holds the result of the execution    
+struct DataInput;  // Holds the entered paths
+struct DataOutput; // Holds the result of the execution
 
 std::ptrdiff_t countFiles(const fs::path &);                            // counts files
 std::tuple<int, int> countLines(const fs::path &);                      // counts empty & non_empty lines
@@ -41,44 +41,32 @@ int main(int argc, char **argv)
         if (argc < 2)
         {
             throw std::invalid_argument("No input path provided");
-            return EXIT_FAILURE;
         };
 
         DataInput Input(std::vector<fs::path>(argv + 1, argv + argc));
         DataOutput Output{};
 
-        for (const auto &path : Input.paths_vector) // calls the countFiles function for all paths
-        {
-            try
-            {
-                Output.files_number += countFiles(path);
-            }
-            catch (std::exception &e)
-            {
-                std::cerr << "Error counting files for path " << path << ": " << e.what() << "\n";
-                return EXIT_FAILURE;
-            }
-        }
-
-        for (const auto &path : Input.paths_vector) // calls the countLines function for all paths
+        for (const auto &path : Input.paths_vector) // calls the countFiles && countLines function for all paths
         {
             try
             {
                 int num_empty_lines, num_non_empty_lines;
+
+                Output.files_number += countFiles(path);
                 std::tie(num_empty_lines, num_non_empty_lines) = countLines(path);
                 Output.num_empty += num_empty_lines;
                 Output.num_non_empty += num_non_empty_lines;
             }
             catch (std::exception &e)
             {
-                std::cerr << "Error counting lines for path " << path << ": " << e.what() << "\n";
+                std::cerr << "Error processing path: " << path << ": " << e.what() << "\n";
                 return EXIT_FAILURE;
             }
         }
 
         std::cout << Output << "\n";
 
-        return 0;
+        return EXIT_SUCCESS;
     }
     catch (std::exception &e)
     {
@@ -126,3 +114,4 @@ std::tuple<int, int> countLines(const fs::path &directory) // counts empty & non
     } });
     return {num_empty, num_non_empty};
 }
+
